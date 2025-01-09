c733a7d1f952089e472884532f96540a1c59a4d3
Harsh Narayan Jha <harshnj@proton.me>
2025-01-09 14:01:12 +0530

    more in the education system

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,9 +3,12 @@
     <div
       class="absolute top-0 bottom-0 m-auto bg-teal-500 opacity-20 w-2/6 aspect-square rounded-full blur-[96px]"></div>
     <h1
-      class="mt-30 z-10 text-8xl py-4 backdrop-blur-sm font-mono font-black tracking-tighter bg-gradient-to-r bg-clip-text text-transparent from-green-300 via-zinc-800 via-40% to-zinc-900 dark:from-stone-700 dark:via-zinc-200 dark:via-30% dark:to-zinc-50"
+      class="mt-30 z-10 text-8xl py-4 backdrop-blur-sm font-mono font-black tracking-tighter bg-gradient-to-r bg-clip-text text-transparent from-zinc-200 via-slate-600 via-40% dark:from-stone-700 dark:via-zinc-200 dark:via-30% dark:to-zinc-50"
       style="mask: linear-gradient(transparent, black 15%, black 85%, transparent)">
-      This is <span>Harsh Narayan Jha</span>
+      This is
+      <span class="bg-gradient-to-r from-green-500 to-sky-500 text-transparent bg-clip-text">
+        Harsh Narayan Jha
+      </span>
     </h1>
     <h2
       class="z-10 backdrop-blur py-4 text-2xl opacity-90 font-semibold mt-8 text-slate-700 dark:text-slate-300"
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="hero" class="mb-20 py-8">
+  <section id="hero" class="py-8 bg-slate-50 dark:bg-gray-950">
     <Hero />
     <Card class="w-9/12 m-auto my-8">
       <template #image>
@@ -21,7 +21,19 @@
     </Card>
   </section>
 
-  <section id="projects" class="mb-20 py-8 px-40">
+  <div class="wave-divider bg-stone-100 dark:bg-gray-900">
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none">
+      <path
+        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+        class="fill-slate-50 dark:fill-gray-950"></path>
+    </svg>
+  </div>
+
+  <section id="projects" class="py-8 px-40 bg-stone-100 dark:bg-gray-900">
     <h1 class="text-4xl font-bold"># Projects&nbsp;&nbsp;🛠</h1>
     <div class="grid grid-cols-3 py-10 px-15">
       <Card
@@ -44,6 +56,42 @@
           </div>
         </template>
       </Card>
+    </div>
+  </section>
+
+  <div class="wave-divider bg-slate-50 dark:bg-gray-950">
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none">
+      <path
+        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+        class="fill-stone-100 dark:fill-gray-900"></path>
+    </svg>
+  </div>
+
+  <section id="education" class="py-8 px-40 bg-slate-50 dark:bg-gray-950">
+    <h1 class="text-4xl font-bold"># Education&nbsp;&nbsp;📚</h1>
+
+    <p class="mb-4 mt-8 font-medium">Not been to a lot of places, but been to the best places.</p>
+    <div class="container flex flex-col-reverse relative">
+      <div class="absolute left-0 top-0 h-full w-3 rounded-full bg-green-400"></div>
+
+      <div class="bg-sky-600 p-4 rounded-xl my-4 w-96 ml-8 shadow-md text-sky-50">
+        <h3 class="font-semibold text-lg mb-1">First School Ever</h3>
+        <p class="leading-tight text-justify">Rose Public School - Laheriasarai, Darbhanga</p>
+      </div>
+
+      <div class="bg-sky-600 p-4 rounded-xl my-4 w-96 ml-8 shadow-md text-sky-50">
+        <h3 class="font-semibold text-lg mb-1">Then a new School</h3>
+        <p class="leading-tight text-justify">Kendriya Vidyalaya No. 1, AFS, Darbhanga</p>
+      </div>
+
+      <div class="bg-sky-600 p-4 rounded-xl my-4 w-96 ml-8 shadow-md text-sky-50">
+        <h3 class="font-semibold text-lg mb-1">Currently at</h3>
+        <p class="leading-tight text-justify">Birla Institute of Technology</p>
+      </div>
     </div>
   </section>
 </template>
