137b983737d472d107e742ab8c36a5d5bd7afa65
Harsh Narayan Jha <harshnj@proton.me>
2025-07-05 21:59:28 +0530

    update resume and consolidate data to one place

---
+++ src/utils/data.ts
@@ -0,0 +1,14 @@
+export const socialLinks = [
+  { name: "Github", path: "https://github.com/HarshNarayanJha", icon: "logos/github" },
+  { name: "Linkedin", path: "https://linkedin.com/in/HarshNarayanJha", icon: "logos/linkedin" },
+  { name: "Leetcode", path: "https://leetcode.com/u/HarshNarayanJha", icon: "logos/leetcode" },
+]
+
+export const accounts = [
+  { name: "Code::Stats", path: "https://codestats.net/users/harshnj" },
+  { name: "TryHackMe", path: "https://tryhackme.com/p/harshnj1103" },
+  { name: "Leetcode", path: "https://leetcode.com/u/HarshNarayanJha" },
+  { name: "CSES", path: "https://cses.fi/user/345212" }
+]
+
+export const RESUME_DOC_URI = "https://docs.google.com/document/d/1lc73m1GYgfdQgenc_bOOsHIVTeNUh-HHB7NJ-QmjXII/edit?usp=sharing"
