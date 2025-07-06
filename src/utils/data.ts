42c64bf3aac6a11a482465a21f0f7ec7cdc1ae50
Harsh Narayan Jha <harshnj@proton.me>
2025-07-06 14:44:55 +0530

    "x mins read" in blogs

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -5,10 +5,46 @@ export const socialLinks = [
 ]
 
 export const accounts = [
-  { name: "Code::Stats", path: "https://codestats.net/users/harshnj" },
-  { name: "TryHackMe", path: "https://tryhackme.com/p/harshnj1103" },
+  { name: "Github", path: "https://github.com/HarshNarayanJha" },
   { name: "Leetcode", path: "https://leetcode.com/u/HarshNarayanJha" },
-  { name: "CSES", path: "https://cses.fi/user/345212" }
+  { name: "Code::Stats", path: "https://codestats.net/users/harshnj" },
+  { name: "CSES", path: "https://cses.fi/user/345212" },
+  { name: "TryHackMe", path: "https://tryhackme.com/p/harshnj1103" }
+]
+
+export const skills = [
+  {
+    name: "Python",
+    tooltip: "First Computer Language learnt. I can make anything with this. My favorite!",
+    logo: "skills/python",
+  },
+  { name: "Django", tooltip: "My goto web fullstack framework before NextJS", logo: "skills/django" },
+  { name: "Flutter", tooltip: "This is what I make mobile Apps in!", logo: "skills/flutter" },
+  {
+    name: "C++",
+    tooltip: "Super performant programming language for low level stuff, including C",
+    logo: "skills/cpp",
+  },
+  { name: "Rust", tooltip: "Learning Rust since it's awesome!", logo: "skills/rust" },
+  { name: "TypeScript", tooltip: "I think TypeScript is better than JavaScript", logo: "skills/ts" },
+  { name: "JavaScript", tooltip: "But JavaScript is also great!", logo: "skills/js" },
+  { name: "Unity", tooltip: "Used to use Unity, before it went downhill with stuff", logo: "skills/unity" },
+  { name: "Godot", tooltip: "I make games in Godot now", logo: "skills/godot" },
+  { name: "Next.js", tooltip: "It is just amazing, but not for everything!", logo: "skills/next" },
+  { name: "Vue.js", tooltip: "Vue is nice!", logo: "skills/vue" },
+  { name: "MongoDB", tooltip: "Just, used...", logo: "skills/mongo" },
+  { name: "Docker", tooltip: "Just used...", logo: "skills/docker" },
+  { name: "AWS", tooltip: "Too difficult to fully master", logo: "skills/aws" },
+  {
+    name: "Tailwind",
+    tooltip: "Before you actually use it, you think it will increase your work. But...",
+    logo: "skills/tailwind",
+  },
+  { name: "Supabase", tooltip: "Supabase is Awesome, much better than AWS for simple stuff", logo: "skills/supabase" },
+  { name: "Blender", tooltip: "Made a lot of stuff in blender", logo: "skills/blender" },
+  { name: "Arch Linux", tooltip: "My primary Operating System, Arch Linux. I love it", logo: "skills/arch" },
+  { name: "Git", tooltip: "Thank god Linus made git. Also thanks for Linux ❤️", logo: "skills/git" },
+  { name: "Zed", tooltip: "Zed, The IDE I use. Much better than VSCode", logo: "skills/zed" },
 ]
 
 export const RESUME_DOC_URI = "https://docs.google.com/document/d/1lc73m1GYgfdQgenc_bOOsHIVTeNUh-HHB7NJ-QmjXII/edit?usp=sharing"
