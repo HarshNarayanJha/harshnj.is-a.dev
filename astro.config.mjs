daad6adfde739c9d119231594ce6ff65a33936e5
Harsh Narayan Jha <harshnj@proton.me>
2025-07-08 18:07:59 +0530

    configure fonts 🔥

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,64 @@
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
+import fontPicker from "astro-font-picker";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), icon(), mdx()],
-	server: {
-		allowedHosts: ["harshnj-garuda.tailf85ff4.ts.net"],
-	},
+  integrations: [svelte(), icon(), mdx(), fontPicker()],
+  server: {
+    allowedHosts: ["harshnj-garuda.tailf85ff4.ts.net"],
+  },
 
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  experimental: {
+    fonts: [
+      {
+        name: "Plein",
+        cssVariable: "--font-plein",
+        provider: fontProviders.fontshare(),
+        weights: ["800", "900"],
+      },
+      {
+        name: "Satoshi",
+        cssVariable: "--font-satoshi",
+        provider: fontProviders.fontshare(),
+        weights: ["400", "500", "600", "700", "800"],
+      },
+      {
+        name: "Switzer",
+        cssVariable: "--font-switzer",
+        provider: fontProviders.fontshare(),
+        weights: ["400", "500", "600", "700", "800"],
+      },
+      {
+        name: "Array",
+        cssVariable: "--font-array",
+        provider: fontProviders.fontshare(),
+        weights: ["600"],
+      },
+      {
+        name: "Architects Daughter",
+        cssVariable: "--font-architects-daughter",
+        provider: fontProviders.google(),
+        weights: ["400"],
+      },
+      {
+        name: "Monaspace Argon",
+        cssVariable: "--font-monaspace-argon",
+        provider: fontProviders.fontsource(),
+        weights: ["400", "500", "600", "700", "800"],
+      }
+
+    ]
+  },
+
+  // markdown: {
+  //   // remarkPlugins: [[remarkToc, { heading: "In this Blog", maxDepth: 3 }]]
+  // },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
