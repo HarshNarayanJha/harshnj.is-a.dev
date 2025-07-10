6a9c2430723f7138f12e7abe7688da262a755da5
Harsh Narayan Jha <harshnj@proton.me>
2025-07-10 17:09:23 +0530

    add sitemap + robots.txt + begin deployement config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
@@ -9,11 +10,11 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), icon(), mdx(), fontPicker()],
+  integrations: [svelte(), icon(), mdx(), fontPicker(), sitemap()],
   server: {
     allowedHosts: ["harshnj-garuda.tailf85ff4.ts.net"],
   },
-
+  site: "https://harshnj.is-a.dev",
   experimental: {
     fonts: [
       {
@@ -51,9 +52,8 @@ export default defineConfig({
         cssVariable: "--font-monaspace-argon",
         provider: fontProviders.fontsource(),
         weights: ["400", "500", "600", "700", "800"],
-      }
-
-    ]
+      },
+    ],
   },
 
   // markdown: {
